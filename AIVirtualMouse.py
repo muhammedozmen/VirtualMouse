472c496e40603dbe4043efd0ccf799c4e562b4f1
Muhammed Ozmen <m.ozmenn34@gmail.com>
2024-02-01 19:20:05 +0300

    Virtual Moving Mode

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -10,7 +10,9 @@ cap = cv2.VideoCapture(0)
 cap.set(3, w_cam)
 cap.set(4, h_cam)
 p_time = 0
-detector = ht.handDetector(maxHands=2)
+detector = ht.handDetector(maxHands=1)
+wScr, hScr = autopy.screen.size()
+#print(wScr,hScr)
 
 while True:
     # 1. Find hand Landmarks
@@ -19,14 +21,28 @@ while True:
     lmList, bbox = detector.findPosition(img)
 
     # 2. Get the tip of the index and middle fingers
-    # 3. Check that which fingers are up
-    # 4. Only Index Finger : Moving Mode
-    # 5. Convert Coordinates
-    # 6. Smoothen Values
-    # 7. Move Mouse
-    # 8. Both Index and middle fingers are up : Clicking Mode
-    # 9. Find distance between fingers
-    # 10. Click mouse if distance is short
+    if len(lmList) !=0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        #print(x1, y1, x2, y2)
+
+        # 3. Check that which fingers are up
+        fingers = detector.fingersUp()
+        #print(fingers)
+
+        # 4. Only Index Finger : Moving Mode
+        if fingers[1] == 1 and fingers[2] == 0:
+
+            # 5. Convert Coordinates
+            x3 = np.interp(x1, (0, w_cam), (0, wScr))
+            y3 = np.interp(y1, (0, h_cam), (0, hScr))
+
+            # 6. Smoothen Values
+            # 7. Move Mouse
+            autopy.mouse.move(x3, y3)
+        # 8. Both Index and middle fingers are up : Clicking Mode
+        # 9. Find distance between fingers
+        # 10. Click mouse if distance is short
 
     # 11. Frame Rate
     c_time = time.time()
