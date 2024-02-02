2956724df3009eee313621da33191459b97dd6ec
Muhammed Ozmen <m.ozmenn34@gmail.com>
2024-02-02 23:03:00 +0300

    Virtual Clicking Mode & Bug Fixes

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -5,14 +5,19 @@ import time
 import autopy
 
 w_cam, h_cam = 640, 480
+frame_r = 100  # Frame Reduction
+smoothening = 6
+
+p_time = 0
+p_locX, p_locY = 0, 0  # Previous Locations
+c_locX, c_locY = 0, 0  # Current Locations
 
 cap = cv2.VideoCapture(0)
 cap.set(3, w_cam)
 cap.set(4, h_cam)
-p_time = 0
 detector = ht.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
-#print(wScr,hScr)
+# print(wScr,hScr)
 
 while True:
     # 1. Find hand Landmarks
@@ -21,35 +26,54 @@ while True:
     lmList, bbox = detector.findPosition(img)
 
     # 2. Get the tip of the index and middle fingers
-    if len(lmList) !=0:
+    if len(lmList) != 0:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
-        #print(x1, y1, x2, y2)
+        # print(x1, y1, x2, y2)
 
         # 3. Check that which fingers are up
         fingers = detector.fingersUp()
-        #print(fingers)
+        # print(fingers)
+        cv2.rectangle(img, (frame_r, frame_r), (w_cam - frame_r, h_cam - frame_r),
+                      (255, 0, 255), 2)
 
         # 4. Only Index Finger : Moving Mode
         if fingers[1] == 1 and fingers[2] == 0:
 
             # 5. Convert Coordinates
-            x3 = np.interp(x1, (0, w_cam), (0, wScr))
-            y3 = np.interp(y1, (0, h_cam), (0, hScr))
+            x3 = np.interp(x1, (frame_r, w_cam - frame_r), (0, wScr))
+            y3 = np.interp(y1, (frame_r, h_cam - frame_r), (0, hScr))
 
             # 6. Smoothen Values
+            c_locX = p_locX + (x3 - p_locX) / smoothening
+            c_locY = p_locY + (y3 - p_locY) / smoothening
+
             # 7. Move Mouse
-            autopy.mouse.move(x3, y3)
+            autopy.mouse.move(wScr - c_locX, c_locY)
+            cv2.circle(img, (x1, y1), 9, (255, 0, 255),
+                       cv2.FILLED)  # The circle on the finger when moving mode is activated
+            p_locX, p_locY = c_locX, c_locY
+
         # 8. Both Index and middle fingers are up : Clicking Mode
-        # 9. Find distance between fingers
-        # 10. Click mouse if distance is short
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0:
+
+            # 9. Find distance between fingers
+            length, img, line_info = detector.findDistance(8, 12,
+                                                           img)  # Find distance between 2 fingers when clicking mode is activated
+            print(length)
+
+            # 10. Click mouse if distance is short
+            if length < 21:
+                cv2.circle(img, (line_info[4], line_info[5]),
+                           9, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()
 
     # 11. Frame Rate
     c_time = time.time()
-    fps = 1/(c_time-p_time)
+    fps = 1 / (c_time - p_time)
     p_time = c_time
-    cv2.putText(img, str(int(fps)), (20,50), cv2.FONT_HERSHEY_PLAIN, 3,
-                (255,0,0), 3)
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
     # 12. Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
